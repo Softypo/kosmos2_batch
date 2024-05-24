e95e919a0fa09b0b01779fdbc3dd69793041af5b
Softypo <ggrsoft@gmail.com>
2024-05-23 18:08:36 -0700

    first commit

---
+++ test.py
@@ -0,0 +1,50 @@
+import requests
+
+from PIL import Image
+from transformers import AutoProcessor, AutoModelForVision2Seq
+
+
+model = AutoModelForVision2Seq.from_pretrained("microsoft/kosmos-2-patch14-224")
+processor = AutoProcessor.from_pretrained("microsoft/kosmos-2-patch14-224")
+
+url = "https://huggingface.co/microsoft/kosmos-2-patch14-224/resolve/main/snowman.png"
+image = Image.open(requests.get(url, stream=True).raw)
+image = Image.open("path")
+
+def run_example(prompt):
+
+    inputs = processor(text=prompt, images=image, return_tensors="pt")
+    generated_ids = model.generate(
+      pixel_values=inputs["pixel_values"],
+      input_ids=inputs["input_ids"],
+      attention_mask=inputs["attention_mask"],
+      image_embeds=None,
+      image_embeds_position_mask=inputs["image_embeds_position_mask"],
+      use_cache=True,
+      max_new_tokens=128,
+    )
+    generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+    _processed_text = processor.post_process_generation(generated_text, cleanup_and_extract=False)
+    processed_text, entities = processor.post_process_generation(generated_text)
+
+    print(processed_text)
+    print(entities)
+    print(_processed_text)
+
+# prompt = "<grounding><phrase> a girl</phrase>"
+# run_example(prompt)
+
+# prompt = "<grounding> Question: What is special about this image? Answer:"
+# run_example(prompt)
+
+# prompt = "<grounding> Question: Where is<phrase> the girl</phrase>? Answer:"
+# run_example(prompt)
+
+# prompt = "<grounding> An image of"
+# run_example(prompt)
+
+prompt = "<grounding> Describe this image in detail and tell what is in the image:"
+run_example(prompt)
+
+
+
